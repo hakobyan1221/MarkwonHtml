c52aa2d5041845216182aad323c577dd38b5041c
Maga <margarita.hakobyan@sololearn.com>
2021-11-29 15:06:19 +0400

    initial commit

---
+++ markwon-test-span/build/generated/source/buildConfig/release/io/noties/markwon/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package io.noties.markwon.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "io.noties.markwon.test";
+  public static final String BUILD_TYPE = "release";
+}
